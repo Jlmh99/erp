54e3e09fd927fc4235a9e5c9b5837332a242591c
Jlmh99 <juanluismh1999@gmail.com>
2026-02-20 18:30:17 -0600

    feat: inicializacion del proyecto

---
+++ src/app/app.config.ts
@@ -0,0 +1,19 @@
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import Aura from '@primeng/themes/aura';
+import { providePrimeNG } from 'primeng/config';
+
+import { routes } from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZonelessChangeDetection(),
+    providePrimeNG({
+        theme: {
+            preset: Aura // Aquí es donde ocurre la magia del diseño
+        }
+    }),
+    provideRouter(routes)
+  ]
+};
