36037b8df041c42eea237056772253168eb75282
Jlmh99 <juanluismh1999@gmail.com>
2026-02-27 08:38:56 -0600

    guardando cambios

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,15 @@
+import { provideZonelessChangeDetection } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import Lara from '@primeng/themes/lara';
 import { providePrimeNG } from 'primeng/config';
-import 'zone.js';
 
 import { App } from './app/app';
 import { routes } from './app/app.routes';
 
 bootstrapApplication(App, {
   providers: [
+    provideZonelessChangeDetection(),
     provideRouter(routes),
     providePrimeNG({
       theme: {
