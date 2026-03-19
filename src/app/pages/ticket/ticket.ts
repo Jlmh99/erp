74aeb8ec9634f9dd7751f5e071b1d0bffa4bf780
Jlmh99 <122195336+Jlmh99@users.noreply.github.com>
2026-03-19 14:23:43 -0600

    ticket con las validaciones

--- src/app/pages/ticket/ticket.ts
+++ src/app/pages/ticket/ticket.ts
@@ -1,9 +1,11 @@
 import { CommonModule, DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MessageService } from 'primeng/api';
 import { ButtonModule } from 'primeng/button';
 import { DialogModule } from 'primeng/dialog';
 import { InputTextModule } from 'primeng/inputtext';
+import { ToastModule } from 'primeng/toast';
 
 export interface Ticket {
   id: number;
@@ -26,10 +28,13 @@ export interface Ticket {
   imports: [
     CommonModule,FormsModule,
     ButtonModule,DialogModule,
-    InputTextModule,DatePipe
-  ]
+    InputTextModule,DatePipe,ToastModule
+  ],
+  providers: [MessageService]
 })
 export class TicketComponent {
+
+  constructor(private messageService: MessageService) {}
 
   tickets: Ticket[] = [];
 
@@ -45,26 +50,57 @@ export class TicketComponent {
     this.mostrarCreate = true;
   }
 
-  crearTicket() {
-    const ticket: Ticket = {
-      id: Date.now(),
-      titulo: this.nuevoTicket.titulo || '',
-      descripcion: this.nuevoTicket.descripcion || '',
-      estado: this.nuevoTicket.estado || 'Pendiente',
-      asignadoA: this.nuevoTicket.asignadoA || '',
-      prioridad: this.nuevoTicket.prioridad || 'Media',
-      fechaCreacion: new Date(),
-      fechaLimite: this.nuevoTicket.fechaLimite || '',
-      comentarios: [],
-      historial: ['Ticket creado']
-    };
+  estados = ['Pendiente', 'En progreso', 'Revision', 'Finalizado'];
 
-    this.tickets.push(ticket);
-    this.mostrarCreate = false;
+  prioridades = ['Baja', 'Media', 'Alta'];
+
+  crearTicket(form: any) {
+
+  // validación extra (trim)
+  if (
+    !this.esTextoValido(this.nuevoTicket.titulo) ||
+    !this.esTextoValido(this.nuevoTicket.descripcion) ||
+    !this.esTextoValido(this.nuevoTicket.asignadoA)
+  ) {
+    this.messageService.add({
+      severity: 'error',
+      summary: 'Error',
+      detail: 'No se permiten campos vacíos o solo espacios'
+    });
+   
+    return;
   }
+
+  if (form.invalid) {
+    Object.values(form.controls).forEach((control: any) => {
+      control.markAsTouched();
+    });
+    return;
+  }
+
+  const ticket: Ticket = {
+    id: Date.now(),
+    titulo: this.nuevoTicket.titulo!.trim(),
+    descripcion: this.nuevoTicket.descripcion!.trim(),
+    estado: this.nuevoTicket.estado!,
+    asignadoA: this.nuevoTicket.asignadoA!.trim(),
+    prioridad: this.nuevoTicket.prioridad!,
+    fechaCreacion: new Date(),
+    fechaLimite: this.nuevoTicket.fechaLimite!,
+    comentarios: [],
+    historial: ['Ticket creado']
+  };
+
+  this.tickets.push(ticket);
+  this.mostrarCreate = false;
+}
 
   verDetalle(ticket: Ticket) {
     this.ticketSeleccionado = ticket;
     this.mostrarDetail = true;
+  }
+
+  esTextoValido(valor: string | undefined): boolean {
+  return !!valor && valor.trim().length > 0;
   }
 }
